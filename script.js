16cd94f79b7697266642f08dc016024ee45243e2
JackSpiers117 <jack.spiers00@gmail.com>
2022-08-19 11:19:02 +1000

    net art initialised and working

---
+++ script.js
@@ -0,0 +1,9 @@
+document.body.style.margin = 0
+document.body.style.overflow = 'hidden'
+function setup() {
+    createCanvas (window.innerWidth, window.innerHeight)
+}
+
+function draw (){
+    background ('pink')
+}
